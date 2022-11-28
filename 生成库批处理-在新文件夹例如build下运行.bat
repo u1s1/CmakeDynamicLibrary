9584a26cdce1ca3cb77c61df25d73a34708a10b3
Lim <53861552+liming467@users.noreply.github.com>
2022-11-28 22:30:04 +0800

    update：改成直接在根目录下运行即可

--- 生成库批处理-在新文件夹例如build下运行.bat
+++ 生成库批处理-在新文件夹例如build下运行.bat
@@ -1,3 +1,13 @@
+pushd %~dp0
+if not exist build (  #判断文件夹build是否存在，不存在则就创建
+    mkdir build
+) else (
+    del /S /Q /F build
+    rd /S /Q build
+    mkdir build
+)
+ 
+cd build
 cmake ..
 cmake --build .
 TIMEOUT /T -1
