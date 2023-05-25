3da635288a3236ae67b615a320a07353af8577ca
zhangdongpeng <zhangdongpeng@zwcad.com>
2023-05-25 17:10:21 +0800

    feat:在使用上，为不同系统添加了头文件宏控制。

--- TestMode/head.h
+++ TestMode/head.h
@@ -1,14 +1,28 @@
 
 #if defined(_WIN32)
-#include <windows.h>
+    #include <windows.h>
+    typedef HMODULE  MODULE_HANDLE;
 #else
-#include <dlfcn.h>
+    #include <dlfcn.h>
+    typedef void *  MODULE_HANDLE;
 #endif
+
 #include "Interface.hpp"
 
 typedef void (*_VOID_)();
 typedef Interface *(Inter)(void);
 
-HMODULE hModule = LoadLibrary("message.dll");
-_VOID_ hello_str = (_VOID_)GetProcAddress(hModule, "hello");
-Inter* pfn=(Inter *)::GetProcAddress(hModule,"NewMessage");
+#if defined(_WIN32)
+    MODULE_HANDLE hModule = LoadLibrary("message.dll");
+#else
+    MODULE_HANDLE hModule = dlopen("message.dll", RTLD_NOW | RTLD_GLOBAL);
+#endif
+
+#if defined(_WIN32)
+    _VOID_ hello_str = (_VOID_)GetProcAddress(hModule, "hello");
+    Inter* pfn=(Inter *)::GetProcAddress(hModule,"NewMessage");
+#else
+    void *fun = dlsym(hModule, "hello");
+    Inter *pfn = (Inter *)dlsym(hModule, "NewMessage");
+#endif
+
--- TestMode/hello-world.cpp
+++ TestMode/hello-world.cpp
@@ -3,11 +3,18 @@
 #include "head.h"
 
 int main() {
+#ifndef _WIN32
+  void (*hello_str)(void);
+  *(void **)(&hello_str) = fun;
+#endif
   hello_str();
   Interface *face = pfn();
   face->ShowMsg();
   face->SetStr("再见，太阳系");
   face->ShowMsg();
-  std::cout << face->GetStr();
+  std::cout << face->GetStr() << std::endl;
+  face->SetStr("我回来了，太阳系");
+  face->ShowMsg();
+  std::cout << face->GetStr() << std::endl;
   return 0;
 }
